bb217124683262521bc6146762b0e570ca64c8b9
Joel Moss <joel@developwithstyle.com>
2023-02-15 11:43:01 +0000

    feat: Added resolve_attributes hook to massage attrs before render

--- test/phlex/view/attributes.rb
+++ test/phlex/view/attributes.rb
@@ -3,6 +3,43 @@
 describe Phlex::HTML do
 	extend ViewHelper
 
+	describe "resolve_attributes hook" do
+		describe "first" do
+			view do
+				def resolve_attributes(**attributes)
+					attributes[:class] = "1"
+					attributes
+				end
+
+				def template
+					div(identical: "attribute") { "one" }
+				end
+			end
+
+			it "renders" do
+				expect(output).to be == %(<div identical="attribute" class="1">one</div>)
+			end
+		end
+
+		describe "second" do
+			view do
+				def resolve_attributes(**attributes)
+					attributes[:class] = "2"
+					attributes
+				end
+
+				def template
+					div(identical: "attribute") { "one" }
+				end
+			end
+
+			it "renders" do
+				expect(view.new.call).to be == %(<div identical="attribute" class="2">one</div>)
+				expect(view.new.call).to be == %(<div identical="attribute" class="2">one</div>)
+			end
+		end
+	end
+
 	with "hash attributes" do
 		view do
 			def template
